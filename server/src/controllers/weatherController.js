ccd3602cf3dd1567eff20e45f252f24ac5b70ba6
saltowl <vladislava-shakirova@mail.ru>
2019-11-19 12:21:30 +0300

    Add logic for /weather endpoint

--- server/src/controllers/weatherController.js
+++ server/src/controllers/weatherController.js
@@ -1,12 +1,41 @@
-const get_weather_by_city_name = (req, res, next) => {
+import axios from 'axios';
 
+const getWeatherByCityName = (req, res, next) => {
+    const cityName = req.query.city;
+
+    if (typeof cityName !== 'string' || cityName.length < 1) {
+        res.status(404).json({ success: false, message: 'City was not provided' });
+    } else {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=96c2fc4713551153e7966978b449861a`)
+            .then(response => {
+                res.json(response.data);
+            })
+            .catch(error => {
+                res.status(404).json({ success: false, message: 'City was not found', error });
+            });
+    }
 };
 
-const get_weather_by_coords = (req, res, next) => {
+const getWeatherByCoords = (req, res, next) => {
+    const long = parseFloat(req.query.long);
+    const lat = parseFloat(req.query.lat);
 
+    if (!isNaN(long) && !isNaN(lat)) {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=96c2fc4713551153e7966978b449861a`)
+            .then(response => {
+                res.json(response.data);
+            })
+            .catch(error => {
+                res.status(400).json({ success: false, message: `Cannot load weather with coords: [${lat}, ${long}]`, error });
+            });
+    } else {
+        res.status(404).json({ success: false, message: 'Coords was not provided in the correct format' });
+    }
 };
 
 export default {
-    get_weather_by_city_name,
-    get_weather_by_coords
+    getWeatherByCityName,
+    getWeatherByCoords
 };
